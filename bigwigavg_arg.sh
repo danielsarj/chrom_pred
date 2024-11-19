0837942050fefee616ee20871567a3f2062a43d1
danielsarj <dnsarj@gmail.com>
2024-11-19 06:20:35 -0600

    add rest of indv ids

--- bigwigavg_arg.sh
+++ bigwigavg_arg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /project/lbarreiro/USERS/daniel/deeplearn_pred/DIY/prediction
 
-IDs=("AF04" "AF06" "AF08")
+IDs=("AF04" "AF06" "AF10" "AF12" "AF14" "AF16" "AF18" "AF20" "AF22" "AF24" "AF26" "AF28" "AF30" "AF34" "AF36" "AF38" "EU03" "EU05" "EU07" "EU09" "EU13" "EU15" "EU17" "EU19" "EU21" "EU25" "EU27" "EU29" "EU33" "EU37" "EU39" "EU41" "EU43" "EU47")
 Conditions=("NI" "Flu")
 
 for i in ${IDs[@]}; do
