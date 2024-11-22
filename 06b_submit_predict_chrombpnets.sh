7540576e3e2c971f0954bd63f5e10f81a9bc3940
danielsarj <dnsarj@gmail.com>
2024-11-22 13:21:35 -0600

    change script location path

--- 06b_submit_predict_chrombpnets.sh
+++ 06b_submit_predict_chrombpnets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-InputIDs=("AF08" "AF10" "AF12" "AF14" "AF16" "AF18" "AF20" "AF22" "AF24" "AF26" "AF28" "AF30" "AF34" "AF36" "AF38" "EU03" "EU05" "EU07" "EU09" "EU13" "EU15" "EU17" "EU19" "EU21" "EU25" "EU27" "EU29" "EU33" "EU37" "EU39" "EU41" "EU43" "EU47")
+InputIDs=("AF04" "AF06" "AF08" "AF10" "AF12" "AF14" "AF16" "AF18" "AF20" "AF22" "AF24" "AF26" "AF28" "AF30" "AF34" "AF36" "AF38" "EU03" "EU05" "EU07" "EU09" "EU13" "EU15" "EU17" "EU19" "EU21" "EU25" "EU27" "EU29" "EU33" "EU37" "EU39" "EU41" "EU43" "EU47")
 
 # sbatch file
-SBATCH_FILE="/project/lbarreiro/USERS/daniel/deeplearn_pred/scripts/run_chrombpnet_arg.sbatch"
+SBATCH_FILE="/project/lbarreiro/USERS/daniel/deeplearn_pred/github/06a_predict_chrombpnet.sbatch"
 
 # log file to keep track of submitted jobs
 LOG_FILE="/project/lbarreiro/USERS/daniel/deeplearn_pred/scripts/submitted_jobs.log"
