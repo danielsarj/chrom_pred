9e685b004a8de2090c6d04a6e75a3f106ef98a9f
danielsarj <dnsarj@gmail.com>
2024-12-09 11:15:08 -0600

    start annotating DA peaks

---
+++ 13_annotateDApeaks.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(data.table)
+library(GenomicRanges)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(ChIPseeker)
+library(org.Hs.eg.db)
+"%&%" <- function(a,b) paste(a,b, sep = "")
+setwd('W:/deeplearn_pred/DIY/prediction')
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+DAfile <- fread('DApeaks_dataframe.txt')
+
+# make model-specific GRange objects
+pred_DA <- DAfile %>% filter(data=='pred', sig==TRUE) %>% dplyr::select(peakID) %>%
+  separate(peakID, into=c('chr', 'start', 'end'), sep=':') %>% makeGRangesFromDataFrame()
+
+real_DA <- DAfile %>% filter(data=='real', sig==TRUE) %>% dplyr::select(peakID) %>%
+  separate(peakID, into=c('chr', 'start', 'end'), sep=':') %>% makeGRangesFromDataFrame()
+
+# annotate peaks
+pred_annot = annotatePeak(pred_DA, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb='org.Hs.eg.db') %>%
+  as.data.frame() %>% mutate(peakID=seqnames%&%':'%&%start%&%':'%&%end)
+real_annot = annotatePeak(real_DA, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb='org.Hs.eg.db') %>%
+  as.data.frame() %>% mutate(peakID=seqnames%&%':'%&%start%&%':'%&%end)
+
+# get info about peak subsets (real/pred only; intersection)
+pred_peaks <- DAfile %>% filter(data=='pred', sig==TRUE) %>% dplyr::select(peakID) %>% pull()
+real_peaks <- DAfile %>% filter(data=='real', sig==TRUE) %>% dplyr::select(peakID) %>% pull()
+
+pred.only_annot <- pred_annot %>% filter(peakID %in% pred_peaks & !(peakID %in% real_peaks))
+real.only_annot <- real_annot %>% filter(peakID %in% real_peaks & !(peakID %in% pred_peaks))
+pred.real_annot <- pred_annot %>% filter(peakID %in% pred_peaks & peakID %in% real_peaks)
+
+# quick summary
+pred.only_annot$annotation <- gsub("\\(.*?\\)", "", pred.only_annot$annotation)
+pred.only_annot$annotation <- trimws(pred.only_annot$annotation)
+pred.only_summary <- pred.only_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(pred.only_annot))*100) %>%
+  mutate(DApeak='pred')
+
+real.only_annot$annotation <- gsub("\\(.*?\\)", "", real.only_annot$annotation)
+real.only_annot$annotation <- trimws(real.only_annot$annotation)
+real.only_summary <- real.only_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(real.only_annot))*100) %>%
+  mutate(DApeak='real')
+
+pred.real_annot$annotation <- gsub("\\(.*?\\)", "", pred.real_annot$annotation)
+pred.real_annot$annotation <- trimws(pred.real_annot$annotation)
+pred.real_summary <- pred.real_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(pred.real_annot))*100)  %>%
+  mutate(DApeak='pred&real')
+
+# barplot idk
+merged_summary <- rbind(pred.only_summary, real.only_summary, pred.real_summary)
+ggplot(merged_summary) + geom_col(aes(x=annotation, y=perc, fill=DApeak), position='dodge') +
+  theme_bw() + coord_flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
