8eabb65b7469ee1d47f00ea0f1e8e19ccb4606ef
danielsarj <dnsarj@gmail.com>
2024-12-12 17:00:16 -0600

    use limma instead of DESeq2

--- 12_DApeaks.R
+++ 12_DApeaks.R
@@ -1,38 +1,44 @@
 # load libraries
 library(data.table)
 library(tidyverse)
-library(DESeq2)
+library(limma)
+library(edgeR)
 library(UpSetR)
 "%&%" <- function(a,b) paste(a,b, sep = "")
 setwd('/project/lbarreiro/USERS/daniel/deeplearn_pred/DIY/prediction')
 
 # get Katie's count matrix
-katie_count <- fread('/project/lbarreiro/USERS/daniel/katieData/ATACseq_peaks.filtered.paired.noDup.counts') %>%
+katie_count <- fread('/project/lbarreiro/USERS/daniel/katieData/ATAC/corrected_expression.txt') %>%
   as.data.frame()
-katie_count$PeakID <- gsub('_', ':', katie_count$PeakID)
-row.names(katie_count) <- katie_count$PeakID
-katie_count <- katie_count %>% select(-PeakID)
+katie_count$V1 <- gsub('_', ':', katie_count$V1)
+row.names(katie_count) <- katie_count$V1
+katie_count <- katie_count %>% select(-V1)
+katie_weights <- fread('/project/lbarreiro/USERS/daniel/katieData/ATAC/weights.txt')  %>%
+  as.data.frame()
+katie_weights$V1 <- gsub('_', ':', katie_weights$V1)
+row.names(katie_weights) <- katie_weights$V1
+katie_weights <- katie_weights %>% select(-V1)
 
 # make Katie's metadata
-katie_meta <- colnames(katie_count) %>% as.data.frame()
-colnames(katie_meta) <- 'ids'
-katie_meta$condition <- substr(katie_meta$ids, 6, nchar(katie_meta$ids))
-row.names(katie_meta) <- katie_meta$ids
-katie_meta <- katie_meta %>% select(-ids)
-
-# create Katie's DESeq2 dataset
-katie_dds <- DESeqDataSetFromMatrix(countData=katie_count, colData=katie_meta, design=~condition)
+katie_meta <- fread('/project/lbarreiro/USERS/daniel/katieData/ATACseq_metadata.txt')
+katie_meta <- katie_meta[which(!katie_meta$Condition=='Mock'),]
+katie_meta <- katie_meta[!grepl('EU122', katie_meta$Genotyping_ID),]
+## factorize certain columns from meta data
+katie_meta$Batch <- as.factor(katie_meta$Batch)
+katie_meta$Genotyping_ID <- as.factor(katie_meta$Genotyping_ID)
+katie_meta$Condition <- factor(katie_meta$Condition, levels=c('NI','Flu'))
 
 # run differential analysis
-katie_dds <- DESeq(katie_dds)
+katie_design <- model.matrix(~0+Genotyping_ID+Condition, data=katie_meta)
+katie_design <- katie_design[,colSums(katie_design!=0)>0]
+katie_fit <- lmFit(katie_count, design=katie_design, weights=katie_weights) %>% eBayes()
 
 # get results
-katie_results <- results(katie_dds, contrast=c('condition', 'NI', 'Flu'))
+katie_results <- topTable(katie_fit, coef=ncol(katie_fit), number=Inf)
 
 ###
 
 # get predicted count matrix
-
 # set vectors of indv IDs, the conditions, and different model folds
 ids <- c('AF04','AF06','AF08','AF10','AF12','AF14','AF16','AF18','AF20','AF22',
          'AF24','AF26','AF28','AF30','AF34','AF36','AF38','EU03','EU05',
@@ -68,51 +74,52 @@ for (co in conditions){
 }
 predicted_count <- predicted_count %>% as.data.frame()
 row.names(predicted_count) <- predicted_count$V1
-predicted_count <- predicted_count %>% select(-V1) %>% drop_na()
+predicted_count <- predicted_count %>% select(-V1) %>% drop_na() %>% DGEList() %>% 
+  calcNormFactors() 
 
-# make predicted metadata
-predicted_meta <- colnames(predicted_count) %>% as.data.frame()
-colnames(predicted_meta) <- 'ids'
-predicted_meta$condition <- substr(predicted_meta$ids, 6, nchar(predicted_meta$ids))
-row.names(predicted_meta) <- predicted_meta$ids
-predicted_meta <- predicted_meta %>% select(-ids)
-
-# create predicted DESeq2 dataset
-predicted_dds <- DESeqDataSetFromMatrix(countData=predicted_count, colData=predicted_meta, design=~condition)
-
-# run differential analysis
-predicted_dds <- DESeq(predicted_dds)
+# predicted voom
+predicted_groups <- c(rep('NI', length(ids)), rep('Flu', length(ids))) %>% 
+  factor(levels=c('NI','Flu'))
+predicted_ids <- rep(ids, length(conditions)) %>% as.factor()
+predicted_design <- model.matrix(~0+predicted_ids+predicted_groups)
+predicted_voom <- voom(predicted_count, predicted_design, plot=T)
+predicted_fit <-  lmFit(predicted_voom) %>% eBayes()
 
 # get results
-predicted_results <- results(predicted_dds, contrast=c('condition', 'NI', 'Flu'))
+predicted_results <- topTable(predicted_fit, coef=ncol(predicted_fit), number=Inf)
 
 ###
 
 # compare katie's results to predicted results
 
-# turn DESeq2 objects into dfs and get shared peaks
+# get shared peaks
 katie_results_df <- katie_results %>% as.data.frame() %>% rownames_to_column('peakID')
 predicted_results_df <- predicted_results %>% as.data.frame() %>% rownames_to_column('peakID')
-common_peaks <- inner_join(katie_results_df, predicted_results_df, by=c('peakID')) %>% select(peakID) %>%
-  pull()
+combined_dfs <- inner_join(katie_results_df, predicted_results_df, by=c('peakID'))
+common_peaks <- combined_dfs %>% select(peakID) %>% pull()
+
+ggplot(combined_dfs) + geom_point(aes(x=logFC.x, y=logFC.y)) + 
+  xlab('Measured DA logFC') + ylab('Predicted DA logFC') + geom_abline(slope=1, color='red') +
+  stat_smooth(aes(x=logFC.x, y=logFC.y), method='lm', geom='smooth', formula=y~x) + theme_bw()
+ggsave('predicted.measured.DAlogFCcorrelation.pdf', height=4, width=4)
 
 # filter peaks by pval and log2FC, combine dfs
-katie_results_df <- katie_results_df %>% mutate(data='real', sig=padj<0.05&abs(log2FoldChange)>2) %>% 
+katie_results_df <- katie_results_df %>% mutate(data='real', sig=adj.P.Val<0.05&abs(logFC)>2) %>% 
   filter(peakID %in% common_peaks)
-predicted_results_df <- predicted_results_df %>% mutate(data='pred', sig=padj<0.05&abs(log2FoldChange)>2) %>% 
+predicted_results_df <- predicted_results_df %>% mutate(data='pred', sig=adj.P.Val<0.05&abs(logFC)>2) %>% 
   filter(peakID %in% common_peaks)
 combined_DA <- rbind(katie_results_df, predicted_results_df)
 fwrite(combined_DA, 'DApeaks_dataframe.txt', sep=' ')
 
 # make volcano plots of DA peaks
-ggplot(combined_DA) + geom_point(aes(x=log2FoldChange, y=-log10(padj), color=sig)) +
+ggplot(combined_DA) + geom_point(aes(x=logFC, y=-log10(adj.P.Val), color=sig)) +
   facet_wrap(~data) + theme_bw()
 ggsave('DApeaks_volcanoplots.pdf', height=5, width=8)
 
 # get list of peaks per data type (predicted and measured)
-katie_sig_results <- katie_results_df %>% filter(padj<0.05 & abs(log2FoldChange)>2) %>% 
+katie_sig_results <- katie_results_df %>% filter(adj.P.Val<0.05 & abs(logFC)>2) %>% 
   select(peakID) %>% pull()
-predicted_sig_results <- predicted_results_df %>% filter(padj<0.05 & abs(log2FoldChange)>2) %>% 
+predicted_sig_results <- predicted_results_df %>% filter(adj.P.Val<0.05 & abs(logFC)>2) %>% 
   select(peakID) %>% pull()
 sig_list <- list(pred=predicted_sig_results, real=katie_sig_results)
 
