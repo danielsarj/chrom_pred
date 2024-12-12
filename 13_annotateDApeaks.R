830e0348866f2e937601c58420575c7502d1a096
danielsarj <dnsarj@gmail.com>
2024-12-12 17:25:23 -0600

    add missing lib and reformat heatmaps size legend

--- 13_annotateDApeaks.R
+++ 13_annotateDApeaks.R
@@ -1,20 +1,20 @@
+# load libraries and databases
 library(tidyverse)
 library(data.table)
 library(GenomicRanges)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(ChIPseeker)
+library(ReactomePA)
 library(org.Hs.eg.db)
 library(clusterProfiler)
 "%&%" <- function(a,b) paste(a,b, sep = "")
-setwd('W:/deeplearn_pred/DIY/prediction')
-
+setwd('/project/lbarreiro/USERS/daniel/deeplearn_pred/DIY/prediction')
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 DAfile <- fread('DApeaks_dataframe.txt')
 
 # make model-specific GRange objects
 pred_DA <- DAfile %>% filter(data=='pred', sig==TRUE) %>% dplyr::select(peakID) %>%
   separate(peakID, into=c('chr', 'start', 'end'), sep=':') %>% makeGRangesFromDataFrame()
-
 real_DA <- DAfile %>% filter(data=='real', sig==TRUE) %>% dplyr::select(peakID) %>%
   separate(peakID, into=c('chr', 'start', 'end'), sep=':') %>% makeGRangesFromDataFrame()
 
@@ -35,17 +35,20 @@ pred.real_annot <- pred_annot %>% filter(peakID %in% pred_peaks & peakID %in% re
 # quick summary
 pred.only_annot$annotation <- gsub("\\(.*?\\)", "", pred.only_annot$annotation)
 pred.only_annot$annotation <- trimws(pred.only_annot$annotation)
-pred.only_summary <- pred.only_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(pred.only_annot))*100) %>%
+pred.only_summary <- pred.only_annot %>% group_by(annotation) %>% 
+  summarise(count=n(), perc=(n()/nrow(pred.only_annot))*100) %>%
   mutate(DApeak='pred')
 
 real.only_annot$annotation <- gsub("\\(.*?\\)", "", real.only_annot$annotation)
 real.only_annot$annotation <- trimws(real.only_annot$annotation)
-real.only_summary <- real.only_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(real.only_annot))*100) %>%
+real.only_summary <- real.only_annot %>% group_by(annotation) %>% 
+  summarise(count=n(), perc=(n()/nrow(real.only_annot))*100) %>%
   mutate(DApeak='real')
 
 pred.real_annot$annotation <- gsub("\\(.*?\\)", "", pred.real_annot$annotation)
 pred.real_annot$annotation <- trimws(pred.real_annot$annotation)
-pred.real_summary <- pred.real_annot %>% group_by(annotation) %>% summarise(count=n(), perc=(n()/nrow(pred.real_annot))*100)  %>%
+pred.real_summary <- pred.real_annot %>% group_by(annotation) %>% 
+  summarise(count=n(), perc=(n()/nrow(pred.real_annot))*100)  %>%
   mutate(DApeak='pred&real')
 
 # barplot idk
@@ -55,14 +58,17 @@ ggplot(merged_summary) + geom_col(aes(x=annotation, y=perc, fill=DApeak), positi
 ggsave('peaks.annotation.barplot.pdf', height=6, width=5)
 
 # enriched GO terms
-pred.GO <- enrichGO(pred.only_annot$geneId, org.Hs.eg.db, ont='MF') %>% as.data.frame() %>% mutate(DApeak='pred')
-real.GO <- enrichGO(real.only_annot$geneId, org.Hs.eg.db, ont='MF') %>% as.data.frame() %>% mutate(DApeak='real')
-pred.real.GO <- enrichGO(pred.real_annot$geneId, org.Hs.eg.db, ont='MF') %>% as.data.frame() %>% mutate(DApeak='pred&real')
+pred.GO <- enrichGO(pred.only_annot$geneId, org.Hs.eg.db, ont='MF') %>% 
+  as.data.frame() %>% mutate(DApeak='pred')
+real.GO <- enrichGO(real.only_annot$geneId, org.Hs.eg.db, ont='MF') %>% 
+  as.data.frame() %>% mutate(DApeak='real')
+pred.real.GO <- enrichGO(pred.real_annot$geneId, org.Hs.eg.db, ont='MF') %>% 
+  as.data.frame() %>% mutate(DApeak='pred&real')
 
 # join dfs
 jointGOs <- rbind(pred.GO, real.GO, pred.real.GO) %>% select(Description, FoldEnrichment, p.adjust, DApeak)
 ggplot(jointGOs) + geom_point(aes(x=DApeak, y=Description, color=FoldEnrichment, size=p.adjust)) + 
-  theme_bw()
+  theme_bw() + scale_size(transform='reverse')
 ggsave('GOenrichment.heatmap.pdf', height=7, width=10)
 
 # enriched pathways terms
@@ -73,5 +79,5 @@ pred.real.path <- enrichPathway(pred.real_annot$geneId) %>% as.data.frame() %>% 
 # join dfs
 jointpaths <- rbind(pred.path, real.path, pred.real.path) %>% select(Description, FoldEnrichment, p.adjust, DApeak)
 ggplot(jointpaths) + geom_point(aes(x=DApeak, y=Description, color=FoldEnrichment, size=p.adjust)) + 
-  theme_bw()
+  theme_bw() + scale_size(transform='reverse')
 ggsave('Pathwayenrichment.heatmap.pdf', height=6, width=6)
